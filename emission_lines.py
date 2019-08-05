335d3d8cb7f2de0637ee8a5d8fa7c041f5b72dc9
Ankur Dev <ankurdev93@gmail.com>
2019-08-05 13:38:55 +0100

    Re-binning in progress

--- emission_lines.py
+++ emission_lines.py
@@ -4,21 +4,21 @@ import funcfile_emission as fn  # read function module
 import warnings
 warnings.filterwarnings("ignore")
 #OPR3_10660_blue
-# fits_file_b = './data/OPR3_60557423_blue.fits'  # the input fits filename # good1
-# fits_file_r = './data/OPR3_60557423_red.fits'  # the input fits filename
+fits_file_b = './data/OPR3_60557423_blue.fits'  # the input fits filename # good1
+fits_file_r = './data/OPR3_60557423_red.fits'  # the input fits filename
 # fits_file_b = './data/OPR3_78138835_blue.fits'  # the input fits filename # bad
 # fits_file_r = './data/OPR3_78138835_red.fits'  # the input fits filename
-fits_file_b = './data/OPR3_59990194_blue.fits'  # the input fits filename #good2
-fits_file_r = './data/OPR3_59990194_red.fits'  # the input fits filename
+# fits_file_b = './data/OPR3_59990194_blue.fits'  # the input fits filename #good2
+# fits_file_r = './data/OPR3_59990194_red.fits'  # the input fits filename
 
 # Good List: 60557423 (0.205076), 61504585 (0.3062), 61524537, 59990194 (0.0178),
 
 # fits_file_b = './data/OPR3_78267792_blue.fits'  # the input fits filename
 # fits_file_r = './data/OPR3_78267792_red.fits'  # the input fits filename
 
-# z=0.205076 #good1
+z=0.205076 #good1
 # z = 1.0164 # bad
-z= 0.0178 #good2
+# z= 0.0178 #good2
 
 # z = 1.0166
 file_b = fn.read_fits(fits_file_b)
@@ -105,6 +105,54 @@ y_b = np.correlate(tem_b, b_div, 'full') # better CCF
 # ax5 = fig5.add_subplot(111)
 # ax5.plot(wavelength, tem2)
 
+'''Logspace and Rebinning'''
+e = np.e
+ln = np.log
+# print(log(e))
+log_wvlngth = np.logspace(ln(3676), ln(9594.25), 5745, base = e) # correct
+# print(ln(log_wvlngth[100])-ln(log_wvlngth[99]) )
+# print(ln(log_wvlngth[5600])-ln(log_wvlngth[5599]) )
+print(log_wvlngth[:15])
+logunit_wvlngth = ln(log_wvlngth) # works , correct
+
+bins = np.arange(5745)
+#  start with blue spectra only. map log scale to log_wvlngth and put value in bin[i]. Refer Bull notes
+#  For putting the value in the bin, take weighted mean. At each edge you have to check for fraction of value in that
+#   bin and with that fraction*value
+
+calc_log = np.where(np.logical_and((wavelength_b >= log_wvlngth[3]), (wavelength_b < log_wvlngth[4])))
+# print(wavelength_b[know[-1:]])
+calc_log_index = (np.asarray(calc_log)).flatten()
+print(calc_log)
+print(wavelength_b[calc_log_index])
+frac_r = (log_wvlngth[4] - wavelength_b[calc_log_index[-1]]) / 0.25
+print(wavelength_b[calc_log_index[0]])
+frac_l = (wavelength_b[calc_log_index[0]] - log_wvlngth[3]) / 0.25
+print('fracl',frac_l)
+print('fracr', frac_r) # This frac has to be mul by the next lin index val and put in present log bin
+
+num_sum  = 0
+den_sum = 0
+blue_sum = 0
+
+for i in calc_log_index:
+    num_sum = b_list[i]*(1/np.square(b_list_std[i]))
+    den_sum = (1/np.square(b_list_std[i]))
+
+blue_sum = num_sum/den_sum
+print(blue_sum)
+num_sum = num_sum + frac_l * b_list[calc_log_index[0]-1] * (1 / np.square(b_list_std[calc_log_index[0]-1]))
+den_sum = den_sum + frac_l * (1 / np.square(b_list_std[calc_log_index[0]-1]))
+blue_sum = num_sum/den_sum
+print(blue_sum)
+num_sum = num_sum + frac_r * b_list[calc_log_index[-1]+1] * (1 / np.square(b_list_std[calc_log_index[-1]+1]))
+den_sum = den_sum + frac_r * (1 / np.square(b_list_std[calc_log_index[-1]+1]))
+blue_sum = num_sum/den_sum
+print(blue_sum)
+print(b_list[7:11])
+
+#put these vals in bins
+
 
 fig6 = plt.figure(figsize=(14.4,10.8))
 fn.plot_fig(fig6,221,wavelength_b[:], b_div, label1='Spectrum')
@@ -119,7 +167,7 @@ ax6_3.set_xlabel('Lag (Pixels)')
 ax6_4= fn.plot_fig(fig6,224,xaxis1,y_r)
 ax6_4.set_ylabel('Red - CCF')
 ax6_4.set_xlabel('Lag (Pixels)')
-plt.show()
+# plt.show()
 
 
 
